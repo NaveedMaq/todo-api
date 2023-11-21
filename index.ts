32afb931f5dd09e7b01fe0307a01b140d61e7178
Naveed Maqbool <naveedmaqbool30@gmail.com>
2023-11-21 12:32:58 +0530

    add db config

--- index.ts
+++ index.ts
@@ -1,9 +1,29 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import { DataSource } from 'typeorm';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 
 // Instantiate the express app
 const app: Express = express();
 dotenv.config();
+
+// Parse request body
+app.use(bodyParser.json());
+
+// Use CORS install types as well
+app.use(cors());
+
+// Create database connection
+export const AppDataSource = new DataSource({
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB,
+  synchronize: true,
+});
 
 // Define the server port
 const port = process.env.PORT || 3000;
@@ -13,5 +33,13 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Express + Typescript server');
 });
 
-// Start listening to the requests on the defined port
-app.listen(port, () => console.log('Server started on port', port));
+AppDataSource.initialize()
+  .then(() => {
+    console.log('👍 Datasource connection established');
+
+    // Start listening to the requests on the defined port
+    app.listen(port, () => console.log('👍 Server started on port', port));
+  })
+  .catch((error) => {
+    console.log('💥 Error during datasource initialization', error);
+  });
